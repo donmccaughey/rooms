50c2cad049e5227b47f452a382e8b900c3318a46
Don McCaughey <don@donm.cc>
2015-06-07 17:48:19 -0700

    Reworked rooms graph using indices into the rooms vector.

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,34 @@ mod rooms;
 
 
 fn main() {
-  let rooms = rooms::Rooms::build();
-  println!("There are {} rooms to explore.", rooms.len());
+    let rooms = rooms::Rooms::build();
+    println!("There are {} rooms to explore.", rooms.len());
 
-  for room in rooms.room_refs.iter() {
-    println!("  {}", *room.borrow());
-  }
-
-  println!("Walking north:");
-  let mut weak_room_ref = rooms.main_room_ref();
-  loop {
-    let room_ref = weak_room_ref.upgrade().unwrap();
-    println!("  In {}", *room_ref.borrow());
-    match room_ref.borrow().north {
-      None => {
-        println!("No more rooms!");
-        break;
-      },
-      Some(ref north_room_ref) => weak_room_ref = north_room_ref.clone(),
+    for room in rooms.vec.iter() {
+        println!("  {}", room);
     }
-  }
 
-  let northmost_room_ref = match weak_room_ref.upgrade() {
-    None => panic!("Room has been deallocated"),
-    Some(room_ref) => room_ref,
-  };
-  println!("Walking south from {}:", *northmost_room_ref.borrow());
-  loop {
-    let room_ref = weak_room_ref.upgrade().unwrap();
-    println!("  In {}", *room_ref.borrow());
-    match room_ref.borrow().south {
-      None => {
-        println!("No more rooms!");
-        break;
-      },
-      Some(ref south_room_ref) => weak_room_ref = south_room_ref.clone(),
+    let mut room = rooms.main_room();
+    println!("Starting in {}", room);
+    println!("Walking north:");
+    loop {
+        match room.north {
+            None => {
+                println!("No door to the north");
+                break;
+            },
+            Some(i) => {
+                let optional_room = rooms.vec.get(i);
+                match optional_room {
+                    None => panic!("Missing room at index {}!", i),
+                    Some(next_room) => {
+                        room = next_room;
+                        println!("Entering {}", room)
+                    },
+                }
+            },
+        }
     }
-  }
+    println!("Reached the end.");
 }
 
--- src/rooms.rs
+++ src/rooms.rs
@@ -1,106 +1,68 @@
-use std::cell::RefCell;
 use std::fmt;
-use std::rc::{Rc, Weak};
-
-
-type RoomRef = Rc<RefCell<Room>>;
-type WeakRoomRef = Weak<RefCell<Room>>;
 
 
 pub struct Room {
-  pub description: String,
-  pub north: Option<WeakRoomRef>,
-  pub south: Option<WeakRoomRef>,
-  pub east: Option<WeakRoomRef>,
-  pub west: Option<WeakRoomRef>,
+    pub description: String,
+    pub north: Option<usize>,
+    pub south: Option<usize>,
+    pub east: Option<usize>,
+    pub west: Option<usize>,
 }
 
 
 impl Room {
-  pub fn new(description: &str) -> Room {
-    Room {
-      description: description.to_string(),
-      north: None,
-      south: None,
-      east: None,
-      west: None,
+    pub fn new(description: &str) -> Room {
+        Room {
+            description: description.to_string(),
+            north: None,
+            south: None,
+            east: None,
+            west: None,
+        }
     }
-  }
-
-  pub fn new_ref(description: &str) -> RoomRef {
-    Rc::new(RefCell::new(Room::new(description)))
-  }
 }
 
-impl fmt::Show for Room {
-  fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-    write!(formatter, "{}", self.description)
-  }
-}
 
-impl fmt::String for Room {
-  fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-    write!(formatter, "{}", self.description)
-  }
-}
-
-trait RoomBuilder {
-  fn link_north(&self, room_ref: &RoomRef);
-  fn link_south(&self, room_ref: &RoomRef);
-  fn link_east(&self, room_ref: &RoomRef);
-  fn link_west(&self, room_ref: &RoomRef);
-}
-
-impl RoomBuilder for RoomRef {
-  fn link_north(&self, room_ref: &RoomRef) {
-    self.borrow_mut().north = Some(room_ref.clone().downgrade());
-    room_ref.borrow_mut().south = Some(self.clone().downgrade());
-  }
-
-  fn link_south(&self, room_ref: &RoomRef) {
-    self.borrow_mut().south = Some(room_ref.clone().downgrade());
-    room_ref.borrow_mut().north = Some(self.clone().downgrade());
-  }
-
-  fn link_east(&self, room_ref: &RoomRef) {
-    self.borrow_mut().east = Some(room_ref.clone().downgrade());
-    room_ref.borrow_mut().west = Some(self.clone().downgrade());
-  }
-
-  fn link_west(&self, room_ref: &RoomRef) {
-    self.borrow_mut().west = Some(room_ref.clone().downgrade());
-    room_ref.borrow_mut().east = Some(self.clone().downgrade());
-  }
+impl fmt::Display for Room {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{}", self.description)
+    }
 }
 
 
 pub struct Rooms {
-  pub room_refs: Vec<RoomRef>,
+    pub vec: Vec<Room>,
 }
 
 
 impl Rooms {
-  pub fn build() -> Rooms {
-    let mut rooms = Rooms { 
-      room_refs: Vec::new(),
-    };
+    pub fn build() -> Rooms {
+        let mut rooms = Rooms { 
+            vec: Vec::new(),
+        };
 
-    rooms.room_refs.push(Room::new_ref("room 1"));
-    rooms.room_refs.push(Room::new_ref("room 2"));
-    rooms.room_refs.push(Room::new_ref("room 3"));
+        rooms.vec.push(Room::new("room 1"));
+        rooms.vec.push(Room::new("room 2"));
+        rooms.vec.push(Room::new("room 3"));
 
-    rooms.room_refs[0].link_north(&rooms.room_refs[1]);
-    rooms.room_refs[1].link_north(&rooms.room_refs[2]);
+        rooms.vec[0].north = Some(1);
+        rooms.vec[1].south = Some(0);
 
-    rooms
-  }
+        rooms.vec[1].north = Some(2);
+        rooms.vec[2].south = Some(1);
 
-  pub fn len(&self) -> usize {
-    self.room_refs.len()
-  }
+        rooms
+    }
 
-  pub fn main_room_ref(&self) -> WeakRoomRef {
-    self.room_refs[0].clone().downgrade()
-  }
+    pub fn len(&self) -> usize {
+        self.vec.len()
+    }
+
+    pub fn main_room(&self) -> &Room {
+        match self.vec.first() {
+            None => panic!("First room is missing!"),
+            Some(room) => room,
+        }
+    }
 }
 
